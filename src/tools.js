ed4fb702a80bf05cb29ecf70095def39ae0728c9
Will Soares <willame_soares@hotmail.com>
2018-08-03 01:15:45 -0300

    Fix linter errors in JS files

--- src/tools.js
+++ src/tools.js
@@ -2,21 +2,26 @@ export default [
   {
     id: 1,
     name: 'React',
-    url: 'https://reactjs.org/'
+    url: 'https://reactjs.org/',
   },
   {
     id: 2,
     name: 'Jest',
-    url: 'https://jestjs.io/'
+    url: 'https://jestjs.io/',
   },
   {
     id: 3,
     name: 'PropTypes',
-    url: 'https://www.npmjs.com/package/prop-types'
+    url: 'https://www.npmjs.com/package/prop-types',
   },
   {
     id: 4,
     name: 'Sass',
-    url: 'https://sass-lang.com/'
+    url: 'https://sass-lang.com/',
   },
-]
+  {
+    id: 5,
+    name: 'ESLint',
+    url: 'https://eslint.org/',
+  },
+];
